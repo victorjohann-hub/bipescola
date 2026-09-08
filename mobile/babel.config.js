9c8e1fd91f5a5570fb69e0adfb0ece84b8d159dd
guivconB <guivcon@gmail.com>
2026-09-08 17:41:37 -0300

    chore: reestruturação do projeto (backend e mobile FSD) e remoção do node_modules

---
+++ mobile/babel.config.js
@@ -0,0 +1,23 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@app': './src/app',
+            '@pages': './src/pages',
+            '@widgets': './src/widgets',
+            '@features': './src/features',
+            '@entities': './src/entities',
+            '@shared': './src/shared',
+          },
+        },
+      ],
+    ],
+  };
+};
